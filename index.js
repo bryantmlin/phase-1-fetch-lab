de04ad79265dc312fad135fb178312f2f0f8f5d0
Bryant Lin <bryantmlin@gmail.com>
2023-03-23 11:54:36 -0600

    fetch 1

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json))
+
   
+}
+
+function addPageNumbers(books) {
+  const main = document.querySelector('main')
+  const h2 = document.createElement('h2')
+  let total = 0
+  books.forEach(book => {
+    total+= book.numberOfPages
+    h2.innerHTML = total
+    main.appendChild(h2)
+  })
+
 }
 
 function renderBooks(books) {
